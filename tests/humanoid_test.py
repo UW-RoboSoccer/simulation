1663f1b43acff9f5b2aa7fb72b69a7b927a91be0
alielhor <aaelhor@uwaterloo.ca>
2024-12-03 21:48:39 -0500

    Player testing in process

--- tests/humanoid_test.py
+++ tests/humanoid_test.py
@@ -1,19 +1,25 @@
 from absl.testing import absltest, parameterized
-from soccer_env.core.humanoid import Player
-from soccer_env.core.humanoid import PlayerObservables
+from soccer_env.core.humanoid import Player, PlayerObservables
 
 from dm_control import mjcf
 import numpy as np
 
-class PlayerTest(parameterized.TestCase):
-    def test_player_pos_to_actuations(self):
-        player = Player()
-        random_joint_pos = np.random.uniform(-1, 1, len(player.actuators))
+# class PlayerTest(parameterized.TestCase):
+def test_player_pos_to_actuations():
+    player = Player()
+    player._build()
+    return player.mjcf_model
+    
 
-        joint_limits = zip(*(actuator.joint.range for actuator in player.actuators))
-        lower, upper = (np.array(limit) for limit in joint_limits)
-        pose = (random_joint_pos * (upper - lower) + upper + lower) / 2
-        actuation = player.pose_to_actuation(pose)
+if __name__ == '__main__':
+    head = test_player_pos_to_actuations()
+    print(head)
+        # random_joint_pos = np.random.uniform(-1, 1, len(player.actuators))
 
-        np.testing.assert_allclose(actuation, random_joint_pos)
+        # joint_limits = zip(*(actuator.joint.range for actuator in player.actuators))
+        # lower, upper = (np.array(limit) for limit in joint_limits)
+        # pose = (random_joint_pos * (upper - lower) + upper + lower) / 2
+        # actuation = player.pose_to_actuation(pose)
+
+        # np.testing.assert_allclose(actuation, random_joint_pos)
 
