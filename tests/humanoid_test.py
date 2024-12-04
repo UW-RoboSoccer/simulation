dcb0b69568553adc8df878aada03657a66483ce9
exahn <exahn@uwaterloo.ca>
2024-12-03 23:28:02 -0500

    more player class clean up and physics sim test

--- tests/humanoid_test.py
+++ tests/humanoid_test.py
@@ -4,22 +4,18 @@ from soccer_env.core.humanoid import Player, PlayerObservables
 from dm_control import mjcf
 import numpy as np
 
-# class PlayerTest(parameterized.TestCase):
-def test_player_pos_to_actuations():
+def test_compile_and_step_simulation():
     player = Player()
-    player._build()
-    return player.mjcf_model
-    
+    physics = mjcf.Physics.from_mjcf_model(player.mjcf_model)
+    for _ in range(100):
+        physics.step()
+
+def test_player_observables():
+    player = Player()
+    for observable in player.observables:
+        print(observable)
 
 if __name__ == '__main__':
-    head = test_player_pos_to_actuations()
-    print(head)
-        # random_joint_pos = np.random.uniform(-1, 1, len(player.actuators))
-
-        # joint_limits = zip(*(actuator.joint.range for actuator in player.actuators))
-        # lower, upper = (np.array(limit) for limit in joint_limits)
-        # pose = (random_joint_pos * (upper - lower) + upper + lower) / 2
-        # actuation = player.pose_to_actuation(pose)
-
-        # np.testing.assert_allclose(actuation, random_joint_pos)
+    # test_compile_and_step_simulation()
+    test_player_observables()
 
