8893357c7802b847c3b101d2102f619fd53372a0
exahn <exahn@uwaterloo.ca>
2024-12-13 23:08:50 -0500

    modified humanoid_standup env reward and observation space

--- envs/humanoid_standup.py
+++ envs/humanoid_standup.py
@@ -1,33 +1,75 @@
+import brax
 from brax.envs.base import PipelineEnv, State
 from brax.io import mjcf
 from brax import base
+from brax.base import Transform
 from brax import actuator
 
+import brax.math
 import jax
 from jax import numpy as jp
 
+import math
 import pathlib
 
 class HumanoidStandup(PipelineEnv):
-    def __init__(self, **kwargs):
-        # path = pathlib.Path(__file__).parent / 'assets\humanoid\humanoid.xml'
-        path = r'C:\Users\ethan\robocup\soccer_env\assets\humanoid\humanoid.xml'
-        sys = mjcf.load(str(path))
+    """
+    ### Action Space
 
+    ### Observation Space
+
+    ### Rewards
+
+    Reward consists of two parts:
+
+    - *time-variant reward*: only activated for the last x seconds of the episode
+        - *base height*: the height of the base of the humanoid
+        - *joint position*: how closely the joints are to their default stance positions
+        - *base orientation*: how closely the base is to being upright
+    - *time-invariant reward*: activated for the entire episode
+        - *body collision*: penalize high body collision
+        - *momentum change*: penalize high momentum change
+        - *body yank*: penalize high body yank
+        - *joint velocity*: penalize high joint velocities
+        - *action range*: penalize high actions rate
+        - *torques*: penalize high torques
+        - *accelerations*: penalize high accelerations
+
+    ### Reset
+
+    ### Episode Termination
+    """
+
+    def __init__(self, path, **kwargs):
+        self.stance_height = 1.2    # height of torso when standing
+        self.h_sensitivity = kwargs.get('h_sensitivity', 0.01)  # height sensitivity scaling factor
+        self.q_sensitivity = kwargs.get('q_sensitivity', 0.01)  # joint position sensitivity scaling factor
+        self.reward_weight = kwargs.get('reward_weight', [1, 1, 1, 1, 1, 1])  # reward weight for each reward component
+
+        sys = mjcf.load(str(path))
         super().__init__(sys, backend='mjx', **kwargs)
+
+        # array of key link indices
+        self.B = ['torso', 'head', 'pelvis', 'left_foot', 'right_foot']
+        self.B_links = [self.sys.link[i] for i in range(self.sys.num_links) if self.sys.link_names[i] in self.B]
 
     def reset(self, rng: jp.ndarray) -> State:
         rng, rng1, rng2 = jax.random.split(rng, 3)
 
-        low, hi = -0.01, 0.01
+        # Randomize Joint Positions
         qpos = self.sys.init_q + jax.random.uniform(
-            rng1, (self.sys.q_size(),), minval=low, maxval=hi
-        )
-        qvel = jax.random.uniform(
-            rng2, (self.sys.qd_size(),), minval=low, maxval=hi
+            rng1, (self.sys.q_size(),), minval=-1, maxval=1
         )
 
-        pipeline_state = self.pipeline_init(qpos, qvel)
+        # Randomize Joint Velocities
+        qvel = jax.random.uniform(
+            rng2, (self.sys.qd_size(),), minval=-1, maxval=1
+        )
+
+        # Reset actuator activation states
+        act = jp.zeros(self.sys.act_size())
+
+        pipeline_state = self.pipeline_init(qpos, qvel, act)
         obs = self._get_obs(pipeline_state, jp.zeros(self.sys.act_size()))
         reward, done, zero = jp.zeros(3)
         metrics = {
@@ -37,64 +79,64 @@ class HumanoidStandup(PipelineEnv):
         return State(pipeline_state, obs, reward, done, metrics)
     
     def step(self, state: State, action: jp.ndarray) -> State:
+        # Scale action from [-1, 1] to actuators control range
         action_min = self.sys.actuator.ctrl_range[:, 0]
         action_max = self.sys.actuator.ctrl_range[:, 1]
 
-        action = (action + 1) * (action_max - action_min) * 0.5 + action_min # map action from [-1, 1] to [action_min, action_max]
+        action = (action + 1) * (action_max - action_min) * 0.5 + action_min
 
-        pipeline_state = self.pipeline_step(state.pipeline_state, action)
+        # update pipeline state
+        pipeline_state0 = state.pipeline_state
+        pipeline_state = self.pipeline_step(pipeline_state0, action)
 
-        pos_after = pipeline_state.x.pos[0, 2]  # z coordinate of torso
-        uph_cost = (pos_after - 0) / self.dt
-        quad_ctrl_cost = 0.01 * jp.sum(jp.square(action))
-        # quad_impact_cost is not computed here
+        # Calculate reward
+        reward = self.calculate_reward(pipeline_state, pipeline_state0)
+        reward = jp.dot(reward, self.reward_weight)
 
+        # Update observation
         obs = self._get_obs(pipeline_state, action)
-        reward = uph_cost + 1 - quad_ctrl_cost
-        state.metrics.update(reward_linup=uph_cost, reward_quadctrl=-quad_ctrl_cost)
 
         return state.replace(pipeline_state=pipeline_state, obs=obs, reward=reward)
     
-    def _get_obs(
-        self, pipeline_state: base.State, action: jax.Array
-    ) -> jax.Array:
-        """Observes humanoid body position, velocities, and angles."""
-        position = pipeline_state.q[2:]
-        velocity = pipeline_state.qd
+    def _get_obs(self, pipeline_state: base.State, action: jp.ndarray) -> jp.ndarray:
+        # Get joint states
+        q = pipeline_state.q
+        qd = pipeline_state.qd
 
-        com, inertia, mass_sum, x_i = self._com(pipeline_state)
-        cinr = x_i.replace(pos=x_i.pos - com).vmap().do(inertia)
-        com_inertia = jp.hstack(
-            [cinr.i.reshape((cinr.i.shape[0], -1)), inertia.mass[:, None]]
-        )
+        # Get base states
+        x = pipeline_state.x
+        xd_ang = pipeline_state.xd.ang
 
-        xd_i = (
-            base.Transform.create(pos=x_i.pos - pipeline_state.x.pos)
-            .vmap()
-            .do(pipeline_state.xd)
-        )
-        com_vel = inertia.mass[:, None] * xd_i.vel / mass_sum
-        com_ang = xd_i.ang
-        com_velocity = jp.hstack([com_vel, com_ang])
+        return jp.concatenate([q, qd, x.pos, x.rot, xd_ang, action])
 
-        qfrc_actuator = actuator.to_tau(
-            self.sys, action, pipeline_state.q, pipeline_state.qd
-        )
+    def _base_height_reward(self, x: Transform) -> jp.ndarray:
+        return math.exp(-math.pow(max(self.stance_height - x.pos[0, 2], 0), 2) / self.h_sensitivity)
 
-        # external_contact_forces are excluded
-        return jp.concatenate([
-            position,
-            velocity,
-            com_inertia.ravel(),
-            com_velocity.ravel(),
-            qfrc_actuator,
-        ])
+    def _joint_pos_reward(self, q: jax.Array) -> jp.ndarray:
+        return math.exp(-jp.sum(jp.square(self.sys.init_q - q)) / (self.q_sensitivity * self.sys.q_size()))
+    
+    def _base_orient_reward(self, x: Transform) -> jp.ndarray:
+        up = jp.array([0, 0, 1])
+        rot_up = brax.math.rotate(up, x.rot[0])
+        return jp.sum(jp.square(rot_up[:2]))
 
-    def _com(self, pipeline_state: base.State) -> jax.Array:
-        inertia = self.sys.link.inertia
-        mass_sum = jp.sum(inertia.mass)
-        x_i = pipeline_state.x.vmap().do(inertia.transform)
-        com = (
-            jp.sum(jax.vmap(jp.multiply)(inertia.mass, x_i.pos), axis=0) / mass_sum
-        )
-        return com, inertia, mass_sum, x_i
+    def _momentum_change_reward(self, qdd: jax.Array) -> jp.ndarray:
+        return jp.sum(jp.square(qdd * self.sys.link.inertia.mass[:, None]))
+    
+    def _joint_vel_reward(self, qd: jax.Array) -> jp.ndarray:
+        return jp.sum(jp.square(qd))
+    
+    def _joint_acc_reward(self, qdd: jax.Array) -> jp.ndarray:
+        return jp.sum(jp.square(qdd))
+
+    def calculate_reward(self, pipeline_state: base.State, pipeline_state0: base.State) -> jp.ndarray:
+        qdd = (pipeline_state.qd - pipeline_state0.qd) / self.dt
+
+        base_height = self._base_height_reward(pipeline_state.x)
+        joint_pos = self._joint_pos_reward(pipeline_state.q)
+        base_orient = self._base_orient_reward(pipeline_state.x)
+        momentum_change = self._momentum_change_reward(qdd)
+        joint_vel = self._joint_vel_reward(pipeline_state.qd)
+        joint_acc = self._joint_acc_reward(qdd)
+
+        return jp.array([base_height, joint_pos, base_orient, momentum_change, joint_vel, joint_acc])
--- main.py
+++ main.py
@@ -17,6 +17,17 @@ mj_data = mujoco.MjData(mj_model)
 mjx_model = mjx.put_model(mj_model)
 mjx_data = mjx.put_data(mj_model, mj_data)
 
+# Randomize quaternion orientation of root joint (humanoid only)
+# indecies 3:7 are the quaternion values of the root joint, indecies 0:3 are the position values of the root joint
+mj_data.qpos[3:7] = jax.random.uniform(jax.random.PRNGKey(time.time_ns()), (4,), jp.float32, minval=-1, maxval=1)
+mj_data.qpos[3:7] /= jp.linalg.norm(mj_data.qpos[3:7])  # Normalize to get a valid quaternion
+
+# Randomize joint positions
+mj_data.qpos[7:] = jax.random.uniform(jax.random.PRNGKey(time.time_ns()), (mj_data.qpos[7:].shape[0],), jp.float32, minval=-1, maxval=1)
+
+# Randomize joint velocities
+mj_data.qvel = jax.random.uniform(jax.random.PRNGKey(time.time_ns()), (mj_data.qvel.shape[0],), jp.float32, minval=-1, maxval=1)
+
 print(mjx_data.qpos.devices())
 
 print('JIT-compiling the step function...')
@@ -26,6 +37,7 @@ print('compilation finished')
 with mujoco.viewer.launch_passive(mj_model, mj_data) as viewer:
     while viewer.is_running():
         # mj_data.ctrl = jax.random.uniform(jax.random.PRNGKey(time.time_ns()), (mj_data.ctrl.shape[0],), jp.float32, minval=-1, maxval=1)
+        mjx_data = mjx_data.replace(xquat=jp.array(mj_data.xquat))
         mjx_data = mjx_data.replace(ctrl=jp.array(mj_data.ctrl), act=jp.array(mj_data.act))
         mjx_data = mjx_data.replace(qpos=jp.array(mj_data.qpos), qvel=jp.array(mj_data.qvel), time=jp.array(mj_data.time))
         mjx_model = mjx_model.tree_replace({
