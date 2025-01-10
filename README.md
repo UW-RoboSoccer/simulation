8a8f4ef7deead579d79cb4fff9a152700a58547d
Ethan Ahn <ethan@tower10labs.com>
2025-01-10 16:26:31 -0500

    cleaned repo, updated readme, and added support for policy inference

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# simulation
+# Brax Sim Environment with MJX
 Official UWRS Repo for MuJoCo Soccer Simulation
+
+## System Requirements
+- WSL Ubuntu 22.04
+- CUDA
+- NVIDIA CUDA capable GPU
+- Python 3.12
+
+## Setup
+Make python virtual environment
+```
+python3 -m venv .venv
+```
+
+Install requirements.txt
+```
+pip install -r requirements.txt
+```
+
+Upgrade JAX package to use CUDA
+```
+pip install --upgrade jax[cuda<version>]
+```
+**Replace <version> with your CUDA version**
+
+Verify setup by running the visualize.py file
+```
+python visualize.py
+```
+
+The result should generate 500 frames of a simulation of a humanoid and then open a viewer that renders these frames on loop. 
+
+Close the window to end the program.
+
+## Development
--- visualize.py
+++ visualize.py
@@ -15,18 +15,34 @@ from IPython.core.display import HTML
 env_name = 'humanoid'
 env = envs.get_environment(env_name)
 
-def gen_rollout(env, n_steps=500):
+def gen_rollout(env, model_path=None, n_steps=500):
+    jit_inference_fn = None
+
+    if model_path:
+        params = model.load_params(model_path)
+        network = ppo_networks.make_ppo_networks(action_size=env.action_size, observation_size=env.observation_size)
+        make_inference_fn = ppo_networks.make_inference_fn(network)
+        inference_fn = make_inference_fn(params)
+        jit_inference_fn = jax.jit(inference_fn)
+
     print('JIT env functions')
     jit_reset = jax.jit(env.reset)
     jit_step = jax.jit(env.step)
     print('JIT finished')
 
-    state = jit_reset(jax.random.PRNGKey(0))
+    rng = jax.random.PRNGKey(0)
+
+    state = jit_reset(rng)
     rollout = [state.pipeline_state]
 
     for i in range(n_steps):
         print('Step', i)
-        ctrl = jp.zeros(env.sys.nu)
+        if jit_inference_fn:
+            act_rng, rng = jax.random.split(rng)
+            obs = env._get_obs(state.pipeline_state)
+            ctrl, _ = jit_inference_fn(obs, act_rng)
+        else:
+            ctrl = jp.zeros(env.sys.nu)
         state = jit_step(state, ctrl)
         rollout.append(state.pipeline_state)
 
