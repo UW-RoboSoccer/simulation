f8694894d83f606eb86ae0b8c73b04584f894719
Dylan Finlay <dylan.finlay33@gmail.com>
2025-02-05 21:04:11 -0500

    added shell script

---
+++ visualize.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Check if /output directory exists
+if [ ! -d "./output" ]; then
+    echo "Error: /output directory does not exist."
+    exit 1
+fi
+
+# Run the visualize.py script
+python3 temp_visualize.py
